3086765c303315cda326934f2aa9a0de44b8d0c5
Tomat05 <wetherill.t@outlook.com>
2023-03-17 21:40:37 +0000

    Made an oopsie

--- win-play-file/src/main.rs
+++ win-play-file/src/main.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("python3").arg("main.py").spawn().expect("Failed to run game");
+    Command::new("py").arg("main.py").spawn().expect("Failed to run game");
 }
