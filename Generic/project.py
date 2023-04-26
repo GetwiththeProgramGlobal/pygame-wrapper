f2332eb833b6cc75a68c6e002d0d3a6eaad54a37
Tomat05 <wetherill.t@outlook.com>
2023-04-26 20:47:01 +0100

    Refactor to separate linux-specific files

--- Generic/project.py
+++ Generic/project.py
@@ -2,11 +2,9 @@ from gwtp import *
 
 WIDTH, HEIGHT = 1280, 720
 
-
 # Called once at the start
 def setup():
     createWindow(WIDTH, HEIGHT, "Your Caption Here")
-
 
 # Called every frame
 def update():
